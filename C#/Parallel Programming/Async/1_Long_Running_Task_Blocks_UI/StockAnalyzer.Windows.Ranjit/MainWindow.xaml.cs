bd7c4c335026c563b53b65c8391447452779e107
BANDA Ranjit <ranjit.banda@hexagon.com>
2020-12-04 15:43:49 +0530

    Parallel Programming new section started and ASYNC

---
+++ C#/Parallel Programming/Async/1_Long_Running_Task_Blocks_UI/StockAnalyzer.Windows.Ranjit/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using StockAnalyzer.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            #region Before loading stock data
+            var watch = new Stopwatch();
+            watch.Start();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+
+            Search.Content = "Cancel";
+            #endregion
+
+            //var lines = File.ReadAllLines(@"StockPrices_Small.csv");
+            var lines = File.ReadAllLines(@"StockPrices_Big.csv");
+
+            var data = new List<StockPrice>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var segments = line.Split(',');
+
+                for (var i = 0; i < segments.Length; i++) segments[i] = segments[i].Trim('\'', '"');
+                var price = new StockPrice
+                {
+                    Ticker = segments[0],
+                    TradeDate = DateTime.ParseExact(segments[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                    Volume = Convert.ToInt32(segments[6], CultureInfo.InvariantCulture),
+                    Change = Convert.ToDecimal(segments[7], CultureInfo.InvariantCulture),
+                    ChangePercent = Convert.ToDecimal(segments[8], CultureInfo.InvariantCulture),
+                };
+                data.Add(price);
+            }
+
+            Stocks.ItemsSource = data.Where(price => price.Ticker == Ticker.Text);
+
+            #region After stock data is loaded
+            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+            Search.Content = "Search";
+            #endregion
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
