dc77842329563488217eca423a09cfb3b05e2b9f
BANDA Ranjit <ranjit.banda@hexagon.com>
2021-02-06 23:40:37 +0530

    Synced with the Actual Product Managment tutorial

--- Angular/Projects/ProductManagement/2-setting-initial-environment/my-pm/src/app/app.component.spec.ts
+++ Angular/Projects/ProductManagement/2-setting-initial-environment/my-pm/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-pm'`, () => {
+  it(`should have as title 'Angular: Getting Started'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-pm');
+    expect(app.title).toEqual('Angular: Getting Started');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-pm app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular: Getting Started!!');
   });
 });
